fe71a2d9116151d00152504a8b01342aadee1546
victor cesario <victor.cesario@hotmail.com>
2025-02-13 09:25:46 -0300

    wip: configuration

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration;
+using System.Collections.Specialized;
+
+string value = ConfigurationManager.AppSettings.Get("DataPath");
+Console.WriteLine($"MySetting: {value}");
+
 Console.ReadLine();
